c3fd2dbdd2b81a9622729fd1d427b12274c298bf
jinholee17 <jinho_lee@brown.edu>
2024-05-14 21:18:01 -0400

    feat: folders organized and files setup

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import "../src/styles/App.css";
+
+/**
+ * Main program that starts the program front end
+ */
+
+/**
+ * A function that reders the entire APP in the top level.
+ *
+ * @returns The entire Map app and its relative components
+ */
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
