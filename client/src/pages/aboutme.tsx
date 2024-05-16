d54f037f5c519fccaef26048ef7bd79538119d36
jinholee17 <jinho_lee@brown.edu>
2024-05-16 19:46:05 -0400

    feat: about me page edited, spotify and pics

--- client/src/pages/aboutme.tsx
+++ client/src/pages/aboutme.tsx
@@ -1,12 +1,57 @@
 import { useState } from "react";
 import Home from "./home";
 
-export default function AboutMe() {
+enum Section {
+  HOME = "HOME",
+  ABOUTME = "ABOUTME",
+}
+
+interface pageProps {
+  setSection: React.Dispatch<React.SetStateAction<Section>>;
+}
+
+export default function AboutMe(props: pageProps) {
   return (
     <div className="aboutme-page">
-      <h1 aria-label="aboutme header" className="aboutme-header">
-        about me
-      </h1>
+      <button
+        className="back-home-button"
+        aria-label="Home Button"
+        onClick={() => props.setSection(Section.HOME)}
+      >
+        -Back to home
+      </button>
+      <div className="aboutme-text">
+        <h1 className="aboutme-title">About me</h1>
+        <p className="aboutme-paragraph">
+          Hi! I was born in South Korea, but moved to Cambridge, MA when I was 6
+          months old. I grew up in the Port neighborhood, and currently reside
+          in Providence.
+        </p>
+        <p className="aboutme-paragraph">
+          I’m a rising junior at Brown University where I am immersed in
+          computer science, literary arts, theatre, and community organizing. I
+          enjoy running, making lattes, reading poetry, and playing connections!
+        </p>
+        <p className="aboutme-paragraph">
+          Ask me about my favorite restaurants in Cambridge or Taylor Swift’s
+          discography!
+        </p>
+        <iframe
+          src="https://open.spotify.com/embed/playlist/6tS9viY0xw6ccuppOcB2bX?utm_source=generator"
+          width="60%"
+          height="175"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        ></iframe>
+      </div>
+      <div className="aboutme-pics">
+        <img className="gates-image" src="/src/assets/images/gates.jpg"></img>
+        <img
+          className="colorado-image"
+          src="/src/assets/images/colorado.jpg"
+        ></img>
+      </div>
     </div>
   );
 }
