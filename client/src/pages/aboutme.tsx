d3f55828c36628e3506c768aeeb9fdec94109764
jinholee17 <jinho_lee@brown.edu>
2024-05-21 21:48:16 -0400

    feat: more responsive about me page

--- client/src/pages/aboutme.tsx
+++ client/src/pages/aboutme.tsx
@@ -16,39 +16,42 @@ interface pageProps {
 export default function AboutMe(props: pageProps) {
   return (
     <div className="aboutme-page">
-      <button
-        className="back-home-button"
-        id="back-home-button"
-        aria-label="Home Button"
-        onClick={() => props.setSection(Section.HOME)}
-      >
-        -Back to home
-      </button>
-      <div className="aboutme-text">
-        <h1 className="aboutme-title">About me</h1>
-        <p className="aboutme-paragraph">
-          Hi! I was born in South Korea, but moved to Cambridge, MA when I was 6
-          months old. I grew up in the Port neighborhood, and currently reside
-          in Providence.
-        </p>
-        <p className="aboutme-paragraph">
-          I’m a rising junior at Brown University where I am immersed in
-          computer science, literary arts, theatre, and community organizing. I
-          enjoy running, making lattes, reading poetry, and playing connections!
-        </p>
-        <p className="aboutme-paragraph">
-          I made this website using Typescript and React, and deployed using
-          Vercel. Ask me about my favorite restaurants in Cambridge or Taylor
-          Swift’s discography!
-        </p>
-        <iframe
-          src="https://open.spotify.com/embed/playlist/6tS9viY0xw6ccuppOcB2bX?utm_source=generator"
-          width="60%"
-          height="175"
-          frameBorder="0"
-          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-          loading="lazy"
-        ></iframe>
+      <div className="aboutme-page-top">
+        <button
+          className="back-home-button"
+          id="back-home-button"
+          aria-label="Home Button"
+          onClick={() => props.setSection(Section.HOME)}
+        >
+          -Back to home
+        </button>
+        <div className="aboutme-text">
+          <h1 className="aboutme-title">About me</h1>
+          <p className="aboutme-paragraph">
+            Hi! I was born in South Korea, but moved to Cambridge, MA when I was
+            6 months old. I grew up in the Port neighborhood, and currently
+            reside in Providence.
+          </p>
+          <p className="aboutme-paragraph">
+            I’m a rising junior at Brown University where I am immersed in
+            computer science, literary arts, theatre, and community organizing.
+            I enjoy running, making lattes, reading poetry, and playing
+            connections!
+          </p>
+          <p className="aboutme-paragraph">
+            I made this website using Typescript and React, and deployed using
+            Vercel. Ask me about my favorite restaurants in Cambridge or Taylor
+            Swift’s discography!
+          </p>
+          <iframe
+            src="https://open.spotify.com/embed/playlist/6tS9viY0xw6ccuppOcB2bX?utm_source=generator"
+            width="60%"
+            height="175"
+            frameBorder="0"
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          ></iframe>
+        </div>
       </div>
       <div className="aboutme-pics">
         <img className="gates-image" src="/static/images/gates.jpg"></img>
