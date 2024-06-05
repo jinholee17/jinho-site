0d2645df04375aeb6e71e6416c406ae238edb3d5
jinholee17 <jinho_lee@brown.edu>
2024-06-05 13:04:11 -0400

    Test: github pages separate branches

---
+++ test/src/main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/index.css";
+import "./styles/home.css";
+import "./styles/aboutme.css";
+import "./styles/portfolio.css";
+import "./styles/projects.css";
+/**
+ * Main function
+ */
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
