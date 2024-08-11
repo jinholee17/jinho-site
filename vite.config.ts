e3b332dfb1658aca4b1effbfe3489e7a99fbc4cb
jinholee17 <jinho_lee@brown.edu>
2024-08-11 12:33:56 -0400

    test: stackoverflow fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/guess-the-wiki/",
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
 });
