c121662d1332a09d713b58d84a9ee3805dd421cb
jinholee17 <jinho_lee@brown.edu>
2024-08-14 15:06:41 -0400

    feat: added routes for page sublinks

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,29 @@
 import "../src/styles/App.css";
 import Website from "./pages/website";
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import About from "./components/AboutMe/AboutMe.tsx";
+import Home from "./components/Home/Home.tsx";
+import "./styles/index.css";
+import "./styles/home.css";
+import "./styles/aboutme.css";
+import "./styles/portfolio.css";
+import "./styles/projects.css";
 /**
- * Main program that starts the program front end
+ * Main function
  */
-
-/**
- * A function that reders the entire APP in the top level.
- *
- * @returns The entire Map app and its relative components
- */
-function App() {
-  return <Website />;
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <div className="App">
+        <div className="App-header">
+          <Routes>
+            <Route path="/" element={<Website />}></Route>
+            <Route path="/about" element={<About />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  </React.StrictMode>
+);
