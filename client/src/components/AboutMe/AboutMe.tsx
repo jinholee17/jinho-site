5773b9616b8daea81e50d1656533ff8b9f10b35e
jinholee17 <jinho_lee@brown.edu>
2024-08-14 15:06:41 -0400

    feat: added routes for page sublinks

---
+++ client/src/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState } from "react";
+
+export default function AboutMe() {
+  return (
+    <React.Fragment>
+      <h1>this is the about me page</h1>;
+    </React.Fragment>
+  );
+}
