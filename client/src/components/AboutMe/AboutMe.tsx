8ec3f4b181f3f912c9825c66bb772a31761f2765
jinholee17 <jinho_lee@brown.edu>
2024-08-14 19:27:33 -0400

    feat: added routes for all portfolio pages

--- client/src/components/AboutMe/AboutMe.tsx
+++ client/src/components/AboutMe/AboutMe.tsx
@@ -1,10 +1,83 @@
 import React from "react";
-import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import NavBar from "../NavBar/NavBar";
+interface pageProps {
+  setSection: React.Dispatch<React.SetStateAction<Section>>;
+}
+enum Section {
+  HOME = "HOME",
+  ABOUTME = "ABOUTME",
+  COSMIC = "COSMIC",
+  LOCKIN = "LOCKIN",
+  IGNITE = "IGNITE",
+}
 
 export default function AboutMe() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <React.Fragment>
-      <h1>this is the about me page</h1>;
+      <NavBar></NavBar>
+      <div className="aboutme-page">
+        <div className="aboutme-page-top">
+          <Link to="/">
+            <button
+              className="back-home-button"
+              id="back-home-button"
+              aria-label="Home Button"
+            >
+              -Back to home
+            </button>
+          </Link>
+          <div className="aboutme-text">
+            <h1 className="aboutme-title">About me</h1>
+            <p className="aboutme-paragraph">
+              Hi! I was born in South Korea, but moved to Cambridge, MA when I
+              was 6 months old. I grew up in the Port neighborhood, and
+              currently reside in Providence.
+            </p>
+            <p className="aboutme-paragraph">
+              I’m a rising junior at Brown University where I am immersed in
+              computer science, literary arts, theatre, and community
+              organizing. I enjoy running, making lattes, reading poetry, and
+              playing connections!
+            </p>
+            <p className="aboutme-paragraph">
+              I made this website using Typescript and React, and deployed using
+              Vercel. Ask me about my favorite restaurants in Cambridge or
+              Taylor Swift’s discography!
+            </p>
+            <iframe
+              src="https://open.spotify.com/embed/playlist/6tS9viY0xw6ccuppOcB2bX?utm_source=generator"
+              width="60%"
+              height="175"
+              frameBorder="0"
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+            ></iframe>
+          </div>
+        </div>
+        <div className="aboutme-pics">
+          <img className="gates-image" src="/static/images/gates.jpg"></img>
+          <img
+            className="colorado-image"
+            src="/static/images/colorado-fun.jpg"
+          ></img>
+        </div>
+        <div className="aboutme-pics">
+          <img
+            className="colorado-smiling-image"
+            src="/static/images/face-reveal-colorado.jpg"
+          ></img>
+          <img className="andy-image" src="/static/images/andyhouse.jpg"></img>
+          <img
+            className="sunflower-image"
+            src="/static/images/sunflowers.jpg"
+          ></img>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
---
+++ client/src/components/Portfolio/Cosmic.tsx
@@ -0,0 +1,99 @@
+import NavBar from "../NavBar/NavBar";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+enum Section {
+  HOME = "HOME",
+  ABOUTME = "ABOUTME",
+  COSMIC = "COSMIC",
+  LOCKIN = "LOCKIN",
+  IGNITE = "IGNITE",
+}
+
+interface pageProps {
+  setSection: React.Dispatch<React.SetStateAction<Section>>;
+}
+
+export default function Cosmic() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div className="cosmic-page">
+      <NavBar></NavBar>
+      <Link to="/">
+        <button
+          className="back-home-button"
+          id="back-home-button"
+          aria-label="Home Button"
+        >
+          -Back to home
+        </button>
+      </Link>
+      <div className="project-text-box">
+        <h1 className="cosmic-title">Cosmic Connections</h1>
+        <p className="bold-cosmic">Hack @ Brown 2024 Hackathon Entry: </p>
+        <p className="cosmic-text">
+          A star sign compatibility generator! Emails a match an AI generated
+          love-letter.
+        </p>
+        <img className="cosmic-image2" src="/static/images/cosmic.png" />
+        <p className="cosmic-text">
+          My teammates were: Alyssa, Audrey, and Lana. This was all four of us's
+          first hackathon, as well as project like this. It was a challenging
+          process navigating full stack development and working as a team in a
+          short period of time to create a larger-scale project. Another
+          challenge was that all of us came into this project with very
+          different skillsets. As someone who had extremely limited front-end
+          experience prior to this, this project was very much a learning curve
+          for me. This made it all the more rewarding in the end!
+        </p>
+        <p className="cosmic-text">
+          I primarily worked on the backend for this project. We calculated the
+          78 combinations of star signs to find each individual compatibility of
+          every pair of signs. I programmed calls to the Open-AI API to access
+          the prompt-to-text model to generate a love letter personalized to the
+          user's person of choice. I then programmed an emailing option to the
+          user's person of choice, using the javax.mail library. We made our
+          website using Typescript, React, and Java.
+        </p>
+      </div>
+      <div className="cosmic-vid-button">
+        <iframe
+          className="cosmic-video"
+          width="320"
+          height="180"
+          src="https://www.youtube.com/embed/MxdVOOskPsU"
+          title="Cosmic Connection"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+        <button
+          className="square-button"
+          aria-label="square button"
+          onClick={() =>
+            window.open(
+              "https://devpost.com/software/cosmic-connection",
+              "_blank"
+            )
+          }
+        >
+          <div className="image-container">
+            <img
+              className="square-image"
+              src="/static/images/square.png"
+              id="square-image"
+            />
+            <div className="text-overlay">
+              <p className="square-text" id="square-text">
+                Check out the devpost here!
+              </p>
+            </div>
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+}
